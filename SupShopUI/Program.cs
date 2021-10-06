7875594cdf9b2e1fbfd2037c1c3a8570bc0865f4
ocrowley1233 <oisin.p.crowley@gmail.com>
2021-10-06 17:28:01 -0400

    first commit

---
+++ SupShopUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SupShopUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SupplyShop/SupplyShop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SupplyShop
+{
+    public class SupplyShop
+    {
+        public list<Store> Inventory { get; set; }
+        
+        public list<Cart> Checkout { get; set; }
+
+
+
+    }
+}
