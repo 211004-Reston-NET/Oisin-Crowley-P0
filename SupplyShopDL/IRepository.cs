0b69b271346f1c912788e90f96227b46cadd439a
ocrowley1233 <oisin.p.crowley@gmail.com>
2021-10-13 09:29:27 -0400

    Fixed Irepo

--- SupplyShopDL/IRepository.cs
+++ SupplyShopDL/IRepository.cs
@@ -6,8 +6,13 @@ namespace SupplyShopDL
 {
     public interface IRepository
     {
+        /// <summary>
+        /// it will add a customers in our database
+        /// </summary>
+        /// <param name="p_cust"> this is the customer we are returning </param>
+        /// <returns> it will </returns>
         Customers AddCustomer(Customers p_cust);
 
-        Customers GetAllCustomers(Customers p_cust);
+        List<Customers> GetAllCustomers();
     }
 }
